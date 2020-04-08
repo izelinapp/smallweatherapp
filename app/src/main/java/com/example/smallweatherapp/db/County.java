6e2fbbc5d713922e448bd84fcaac0a6234273031
izelinapp <izelinapp@163.com>
2020-04-08 15:43:03 +0800

    加入创建数据库和表的各项配置

--- app/src/main/java/com/example/smallweatherapp/db/County.java
+++ app/src/main/java/com/example/smallweatherapp/db/County.java
@@ -2,5 +2,37 @@ package com.example.smallweatherapp.db;
 
 public class County {
     private int id;
-    
+    private String countyName;
+    private String weatherID;
+    private int cityID;
+    public int getId(){
+        return id;
+    }
+    public void setId(int id){
+        this.id=id;
+    }
+
+    public String getCountyName() {
+        return countyName;
+    }
+
+    public void setCountyName(String countyName) {
+        this.countyName = countyName;
+    }
+
+    public String getWeatherID() {
+        return weatherID;
+    }
+
+    public void setWeatherID(String weatherID) {
+        this.weatherID = weatherID;
+    }
+
+    public int getCityID() {
+        return cityID;
+    }
+
+    public void setCityID(int cityID) {
+        this.cityID = cityID;
+    }
 }
